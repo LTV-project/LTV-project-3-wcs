22b915496fcc5a474608bd75f11a7bd07cfc7073
Tamajin <wild.jlq@gmail.com>
2022-09-07 12:31:22 +0200

    ajout des routes pour la table games avec commentaires dans le code

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,24 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const gamesControllers = require("./controllers/gamesControllers");
 
+// GET
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
+router.get("/games", gamesControllers.browse);
+router.get("/games/:id", gamesControllers.read);
+
+// UPDATE
 router.put("/items/:id", itemControllers.edit);
+router.put("/games/:id", gamesControllers.edit);
+
+// CREATE
 router.post("/items", itemControllers.add);
+router.post("/games", gamesControllers.add);
+
+// DELETE
 router.delete("/items/:id", itemControllers.destroy);
+router.delete("/games/:id", gamesControllers.destroy);
 
 module.exports = router;
