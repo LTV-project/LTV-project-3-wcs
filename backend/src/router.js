3f26b93f0f4edffeda3c0b50fb416216aefbfdd6
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-09-08 16:21:10 +0200

    Ajout routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,6 +8,7 @@ const gamesControllers = require("./controllers/gamesControllers");
 const usersControllers = require("./controllers/usersControllers");
 const lobbiesControllers = require("./controllers/lobbiesControllers");
 const travelInfosControllers = require("./controllers/travelInfosControllers");
+const categoryControllers = require("./controllers/categoryControllers");
 
 // GET
 router.get("/items", itemControllers.browse);
@@ -20,6 +21,8 @@ router.get("/lobbies", lobbiesControllers.browse);
 router.get("/lobbies/:id", lobbiesControllers.read);
 router.get("/travel_info", travelInfosControllers.browse);
 router.get("/travel_info/:id", travelInfosControllers.read);
+router.get("/category", categoryControllers.browse);
+router.get("/category/:id", categoryControllers.read);
 
 // UPDATE
 router.put("/items/:id", itemControllers.edit);
@@ -27,6 +30,7 @@ router.put("/games/:id", gamesControllers.edit);
 router.put("/users/:id", usersControllers.edit);
 router.put("/lobbies/:id", lobbiesControllers.edit);
 router.put("/travel_info/:id", travelInfosControllers.edit);
+router.put("/category/:id", categoryControllers.edit);
 
 // CREATE
 router.post("/items", itemControllers.add);
@@ -34,6 +38,7 @@ router.post("/games", gamesControllers.add);
 router.post("/users", usersControllers.add);
 router.post("/lobbies", lobbiesControllers.add);
 router.post("/travel_info", travelInfosControllers.add);
+router.post("/category", categoryControllers.add);
 
 // DELETE
 router.delete("/items/:id", itemControllers.destroy);
@@ -41,5 +46,6 @@ router.delete("/games/:id", gamesControllers.destroy);
 router.delete("/users/:id", usersControllers.destroy);
 router.delete("/lobbies/:id", lobbiesControllers.destroy);
 router.delete("/travel_info/:id", travelInfosControllers.destroy);
+router.delete("/category/:id", categoryControllers.destroy);
 
 module.exports = router;
