c5a77128ce4f1889e99bc2168c36cfbc23a8949e
Tamajin <wild.jlq@gmail.com>
2022-09-14 14:58:49 +0200

    creation du composant navbar admin

---
+++ frontend/src/components/AdminNavbar.jsx
@@ -0,0 +1,13 @@
+export default function AdminNav() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Logo admin</li>
+          <li>Déconnexion</li>
+          <li>Logo logout</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
