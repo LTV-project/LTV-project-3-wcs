6ec2aca153a698cbbb622c00070568c524fcf7fd
Tamajin <wild.jlq@gmail.com>
2022-09-14 15:28:45 +0200

    ajout du composant dashboard avec css sommaire

---
+++ frontend/src/components/DashboardAdmin.jsx
@@ -0,0 +1,12 @@
+export default function DashboardAdmin() {
+    return(
+        <div className="dashboard-wrapper">
+            <ul className="dashboard-items-container">
+                <li className="dashboard-items">Dashboard</li>
+                <li className="dashboard-items">Users</li>
+                <li className="dashboard-items">Games</li>
+                <li className="dashboard-items">Lobbies</li>
+            </ul>
+        </div>
+    )
+}
