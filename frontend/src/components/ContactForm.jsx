4fdf248bb6df9c5882fca7cb69adeaa4f9bcdee8
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-09-14 16:51:38 +0200

    first commit

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,5 +1,90 @@
-function ContactForm() {
-  return <div>ContactForm</div>;
-}
+import axios from "axios";
+import { useState } from "react";
 
-export default ContactForm;
+export default function ContactForm() {
+  const [contact, setContact] = useState({
+    pseudo: "",
+    firstname: "",
+    lastname: "",
+    content: "",
+    email: "",
+  });
+
+  const postContact = () => {
+    axios
+      .post(`${import.meta.env.VITE_BACKEND_URL}/contact`, { ...contact })
+      .then((response) => {
+        console.error(response);
+        console.error(response.data);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Nous contactez</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          postContact();
+        }}
+      >
+        <input
+          type="text"
+          value={contact.lastname}
+          placeholder="Votre Nom"
+          onChange={(e) =>
+            setContact({
+              ...contact,
+              lastname: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={contact.firstname}
+          placeholder="Prénom"
+          onChange={(e) =>
+            setContact({
+              ...contact,
+              firstname: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={contact.pseudo}
+          placeholder="Pseudo"
+          onChange={(e) =>
+            setContact({
+              ...contact,
+              lastname: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={contact.email}
+          placeholder="Votre adresse mail"
+          onChange={(e) =>
+            setContact({
+              ...contact,
+              email: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={contact.content}
+          placeholder="Votre message"
+          onChange={(e) =>
+            setContact({
+              ...contact,
+              content: e.target.value,
+            })
+          }
+        />
+        <input type="submit" value="Envoyer" />
+      </form>
+    </div>
+  );
+}
