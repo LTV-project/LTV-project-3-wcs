08a33828a5e09b068886bb4f559cd74e4b34bd32
FloBB22000 <florence.goslino@sncf.fr>
2022-09-15 14:32:58 +0200

    début CSS

--- frontend/src/components/VisitorNavbar.jsx
+++ frontend/src/components/VisitorNavbar.jsx
@@ -4,10 +4,10 @@ function VisitorNavbar() {
   return (
     <header>
       <div className="header-container">
-      <li className="li-container">LTV</li>
-      <li className="li-container">Accueil</li>
-      <li className="li-container">Contact</li>
-      <li className="li-container">Je me connecte</li>
+        <li className="li-container">LTV</li>
+        <li className="li-container">Accueil</li>
+        <li className="li-container">Contact</li>
+        <li className="li-container">Je me connecte</li>
       </div>
     </header>
   );
