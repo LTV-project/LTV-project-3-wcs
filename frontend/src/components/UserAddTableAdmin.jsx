a58c1f26081acd5700e07650ed91f5275b43d155
Tamajin <wild.jlq@gmail.com>
2022-09-16 16:03:45 +0200

    axio post

--- frontend/src/components/UserAddTableAdmin.jsx
+++ frontend/src/components/UserAddTableAdmin.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function GameAddTableAdmin() {
-  const [addGame, setAddGame] = useState({
-    name: "",
-    category: "",
+export default function UserAddTableAdmin() {
+  const [addUser, setAddUser] = useState({
+    pseudo: "",
+    firstname: "",
+    lastname: "",
+    age: "",
+    email: "",
+    password: "",
   });
-  const postGame = () => {
-    axios.post(`${import.meta.env.VITE_BACKEND_URL}/games/`, {
-      ...addGame,
+  const postUser = () => {
+    axios.post(`${import.meta.env.VITE_BACKEND_URL}/users/`, {
+      ...addUser,
     });
   };
 
@@ -16,7 +20,7 @@ export default function GameAddTableAdmin() {
     <table className="admin-table">
       <thead>
         <tr>
-          <th>Ajouter un jeu</th>
+          <th>Ajouter un utilisateur</th>
         </tr>
       </thead>
       <tbody>
@@ -24,23 +28,63 @@ export default function GameAddTableAdmin() {
           <td>
             <input
               type="text"
-              value={addGame.name}
-              placeholder="Nom du jeu"
-              onChange={(e) => setAddGame({ ...addGame, name: e.target.value })}
+              value={addUser.pseudo}
+              placeholder="Pseudo"
+              onChange={(e) =>
+                setAddUser({ ...addUser, pseudo: e.target.value })
+              }
             />
           </td>
           <td>
             <input
               type="text"
-              value={addGame.category}
-              placeholder="board_game ou card_game"
+              value={addUser.lastname}
+              placeholder="Nom"
               onChange={(e) =>
-                setAddGame({ ...addGame, category: e.target.value })
+                setAddUser({ ...addUser, lastname: e.target.value })
               }
             />
           </td>
           <td>
-            <button type="button" onClick={postGame}>
+            <input
+              type="text"
+              value={addUser.firstname}
+              placeholder="Prénom"
+              onChange={(e) =>
+                setAddUser({ ...addUser, firstname: e.target.value })
+              }
+            />
+          </td>
+          <td>
+            <input
+              type="email"
+              value={addUser.email}
+              placeholder="Courriel"
+              onChange={(e) =>
+                setAddUser({ ...addUser, email: e.target.value })
+              }
+            />
+          </td>
+          <td>
+            <input
+              type="password"
+              value={addUser.password}
+              placeholder="Mot de passe"
+              onChange={(e) =>
+                setAddUser({ ...addUser, password: e.target.value })
+              }
+            />
+          </td>
+          <td>
+            <input
+              type="number"
+              value={addUser.age}
+              placeholder="Âge"
+              onChange={(e) => setAddUser({ ...addUser, age: e.target.value })}
+            />
+          </td>
+          <td>
+            <button type="button" onClick={postUser}>
               Ajouter
             </button>
           </td>
