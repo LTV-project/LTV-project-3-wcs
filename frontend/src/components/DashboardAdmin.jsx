7cf7870791486ec763cb885237d3ce49add36aed
Tamajin <wild.jlq@gmail.com>
2022-09-21 16:35:27 +0200

    composants tableaux admin transférés au compo dashboard et gestion de l'affichage au click

--- frontend/src/components/DashboardAdmin.jsx
+++ frontend/src/components/DashboardAdmin.jsx
@@ -1,12 +1,73 @@
+import { useState } from "react";
+import UsersAdminTable from "@components/UsersAdminTable";
+import LobbiesAdminTable from "@components/LobbiesAdminTable";
+import GamesAdminTable from "@components/GamesAdminTable";
+
 export default function DashboardAdmin() {
+  const [displayManager, setDisplayManager] = useState({
+    users: false,
+    games: false,
+    lobbies: false,
+  });
+
+  const toggleManager = () => {
+    if (displayManager.users === false) {
+      setDisplayManager({
+        ...displayManager,
+        users: true,
+        games: false,
+        lobbies: false,
+      });
+    } else if (displayManager.games === false) {
+      setDisplayManager({
+        ...displayManager,
+        games: true,
+        users: false,
+        lobbies: false,
+      });
+    } else if (displayManager.lobbies === false) {
+      setDisplayManager({
+        ...displayManager,
+        lobbies: true,
+        users: false,
+        games: false,
+      });
+    } else {
+      setDisplayManager(!displayManager);
+    }
+  };
+
   return (
     <div className="dashboard-wrapper">
       <ul className="dashboard-items-container">
-        <li className="dashboard-items">Dashboard</li>
-        <li className="dashboard-items">Users</li>
-        <li className="dashboard-items">Games</li>
-        <li className="dashboard-items">Lobbies</li>
+        <button type="button" className="dashboard-items">
+          Dashboard
+        </button>
+        <button
+          type="button"
+          className="dashboard-items"
+          onClick={toggleManager}
+        >
+          Users
+        </button>
+        <button
+          type="button"
+          className="dashboard-items"
+          onClick={toggleManager}
+        >
+          Games
+        </button>
+        <button
+          type="button"
+          className="dashboard-items"
+          onClick={toggleManager}
+        >
+          Lobbies
+        </button>
       </ul>
+      {displayManager.users ? <UsersAdminTable /> : ""}
+      {displayManager.lobbies ? <LobbiesAdminTable /> : ""}
+      {displayManager.games ? <GamesAdminTable /> : ""}
     </div>
   );
 }
--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,8 +1,5 @@
 import AdminNav from "@components/AdminNavbar";
 import DashboardAdmin from "@components/DashboardAdmin";
-import UsersAdminTable from "@components/UsersAdminTable";
-import LobbiesAdminTable from "@components/LobbiesAdminTable";
-import GamesAdminTable from "@components/GamesAdminTable";
 
 export default function Admin() {
   return (
@@ -10,9 +7,6 @@ export default function Admin() {
       <AdminNav />
       <div className="admin-workflow">
         <DashboardAdmin />
-        <UsersAdminTable />
-        {/* <LobbiesAdminTable /> */}
-        <GamesAdminTable />
       </div>
     </div>
   );
