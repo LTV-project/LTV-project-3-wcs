5930d592cb2891bab1cb06743b11d7b75e6decdc
Tamajin <wild.jlq@gmail.com>
2022-09-27 22:03:09 +0200

    ajout du map

--- frontend/src/components/LobbiesTalkList.jsx
+++ frontend/src/components/LobbiesTalkList.jsx
@@ -2,17 +2,20 @@ import { useNavigate } from "react-router-dom";
 
 export default function LobbiesTalkList({ displayLobbies }) {
   const navigate = useNavigate();
-
   return (
     <div className="dark-body">
-      <div className="mini-card">
-        <p>{displayLobbies.pseudo}</p>
-        <p>Thème</p>
-        <p>Participants</p>
-        <p>Places</p>
-        <button type="button">détails</button>
-        <button type="button">rejoindre</button>
-      </div>
+      {displayLobbies &&
+        displayLobbies.map((lobby) => (
+          <div className="mini-card" key={lobby.id}>
+            {/* <img src="" alt="user-avatar" /> */}
+            <p>Hôte : {lobby.pseudo}</p>
+            <p>Thème : {lobby.theme}</p>
+            <p>Participants : {lobby.participants}</p>
+            <p>Description : {lobby.description}</p>
+            <button type="button">détails</button>
+            <button type="button">rejoindre</button>
+          </div>
+        ))}
       <button
         type="button"
         className="generic-btn return-btn-lobbyChoice"
