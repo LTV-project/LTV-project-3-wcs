06f2fd34f9deb6066d46399b900c4a741c059b92
Tamajin <wild.jlq@gmail.com>
2022-09-28 23:52:56 +0200

    création navbar complete avec affichage à conditionner dans le contexte

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "./Banner";
 
-function VisitorNavbar() {
+function Navbar() {
   return (
     <header>
-      <li>Lorem, ipsum.</li>
-      <li>Tempora, nemo?</li>
-      <li>Blanditiis, odit.</li>
+      <nav>
+        <ul className="navbar">
+          <Link to="/">
+            <li className="logo">LTV</li>
+          </Link>
+          <Link to="/">
+            <li className="nav-item">Accueil</li>
+          </Link>
+          <Link to="/signIn">
+            <li className="nav-item">Se connecter</li>
+          </Link>
+          <Link to="/userProfile/:id">
+            <li className="nav-item">Mon compte</li>
+          </Link>
+          <Link to="/accountCreation">
+            <li className="nav-item">S'inscrire</li>
+          </Link>
+          <Link to="contacts">
+            <li className="nav-item">Contact</li>
+          </Link>
+        </ul>
+      </nav>
+      <Banner />
     </header>
   );
 }
 
-export default VisitorNavbar;
+export default Navbar;
