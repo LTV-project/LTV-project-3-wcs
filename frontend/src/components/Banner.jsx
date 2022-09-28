f5d8e8729b18f6113d1b434b69742236e34a8be2
Tamajin <wild.jlq@gmail.com>
2022-09-28 23:51:51 +0200

    composant banner générique

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,7 +1,9 @@
+import trainBanner from "template-fullstack/assets/images/train_banner.jpg";
+
 function Banner() {
   return (
     <div className="banner">
-      <p className="banner-text">Lorem ipsum dolor sit amet consectetur.</p>
+      <img src={trainBanner} alt="train" />
     </div>
   );
 }
