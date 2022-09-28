2f32a0b632776f90498aa178aa7f44ad563caaa1
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-09-28 10:39:58 +0200

    remove component

--- frontend/src/pages/LobbiesGameCreation.jsx
+++ frontend/src/pages/LobbiesGameCreation.jsx
@@ -1,11 +1,9 @@
 import ChoiceGameCreateLobby from "@components/ChoiceGameCreateLobby";
-// import LobbyGame from "@components/LobbyGame";
 
 function LobbiesGameCreation() {
   return (
     <div>
       <ChoiceGameCreateLobby />
-      {/* <LobbyGame /> */}
     </div>
   );
 }
