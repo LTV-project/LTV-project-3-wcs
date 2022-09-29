8aae26f776e870bd68bd77fdf05ab40923930754
Tamajin <wild.jlq@gmail.com>
2022-09-29 10:07:21 +0200

    revue des routes avec nommage conforme aux conventions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "@pages/Contact";
 import AccountCreation from "@pages/AccountCreation";
 import SignIn from "@pages/SignIn";
-import LobbiesCreation from "@pages/LobbiesCreation";
 import HomePage from "@pages/HomePage";
 import UserProfile from "@pages/UserProfile";
+import LobbyGameCreation from "@pages/LobbyGameCreation";
+import LobbyTalkCreation from "@pages/LobbyTalkCreation";
 import EditAccount from "@components/EditAccount";
 import JoinLobbies from "@pages/JoinLobbies";
 import AdminPage from "@pages/AdminPage";
@@ -20,13 +21,14 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/contacts" element={<Contact />} />
-          <Route path="/joinLobbies" element={<JoinLobbies />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/userProfile" element={<UserProfile />} />
-          <Route path="/userProfile/:id" element={<EditAccount />} />
-          <Route path="/accountCreation" element={<AccountCreation />} />
-          <Route path="/lobbiesCreation" element={<LobbiesCreation />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/join-lobbies" element={<JoinLobbies />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/user-profile/:id" element={<UserProfile />} />
+          <Route path="/user-profile/:id/edit" element={<EditAccount />} />
+          <Route path="/account-creation" element={<AccountCreation />} />
+          <Route path="/create-lobby-game" element={<LobbyGameCreation />} />
+          <Route path="/create-lobby-talk" element={<LobbyTalkCreation />} />
           <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </div>
