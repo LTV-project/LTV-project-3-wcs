2e6cb39324a7e2423b64e2f9799a9286dd0ab9d0
Tamajin <wild.jlq@gmail.com>
2022-09-30 01:20:08 +0200

    ajout du composant des commentaires utilisateurs

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Navbar from "@components/Navbar";
 import UserDashboard from "@components/UserDashboard";
+import UsersComments from "@components/UsersComments";
 
 function HomePage() {
   return (
     <div>
       <Navbar />
       <UserDashboard />
+      <UsersComments />
     </div>
   );
 }
