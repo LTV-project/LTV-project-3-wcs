68f729ee82e3fbba4be00c909cd8998688918aea
Tamajin <wild.jlq@gmail.com>
2022-09-30 15:05:50 +0200

    ajout du map pour gérer l'affichage des utilisateurs

--- frontend/src/components/UsersComments.jsx
+++ frontend/src/components/UsersComments.jsx
@@ -1,29 +1,25 @@
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function UsersComments() {
+  const [userComment, setUserComment] = useState("");
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/images`)
+      .then((response) => response.data)
+      .then((data) => setUserComment(data));
+  }, []);
+
   return (
     <div className="users-comments-flex-container">
-      <div className="user-comment">
-        <img src="https://placekitten.com/80/80" alt="" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa suscipit
-          magnam ratione?
-        </p>
-      </div>
-      <div className="user-comment">
-        <img src="https://placekitten.com/80/80" alt="" />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt libero
-          quae dolorum accusamus, qui enim.
-        </p>
-      </div>
-      <div className="user-comment">
-        <img src="https://placekitten.com/80/80" alt="" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
-          commodi?
-        </p>
-      </div>
+      {userComment &&
+        userComment.map((comment) => (
+          <div className="user-comment" key={comment.id}>
+            <img src={comment.image} alt="" />
+            <p>{comment.pseudo}</p>
+            <p>{comment.description ? comment.description : "Lorem ipsum"}</p>
+          </div>
+        ))}
     </div>
   );
 }
