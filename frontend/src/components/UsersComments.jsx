801c28e183862f2a9f06b17463967079cddfc394
Tamajin <wild.jlq@gmail.com>
2022-09-30 10:40:05 +0200

    composant usersComments

---
+++ frontend/src/components/UsersComments.jsx
@@ -0,0 +1,31 @@
+// import { useEffect } from "react";
+
+function UsersComments() {
+  return (
+    <div className="users-comments-flex-container">
+      <div className="user-comment">
+        <img src="https://placekitten.com/80/80" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa suscipit
+          magnam ratione?
+        </p>
+      </div>
+      <div className="user-comment">
+        <img src="https://placekitten.com/80/80" alt="" />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt libero
+          quae dolorum accusamus, qui enim.
+        </p>
+      </div>
+      <div className="user-comment">
+        <img src="https://placekitten.com/80/80" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+          commodi?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default UsersComments;
