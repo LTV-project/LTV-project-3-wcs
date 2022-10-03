68031d2109da6a0ace5571de67486178352d75fd
FloBB22000 <florence.goslino@sncf.fr>
2022-10-03 16:39:16 +0200

    css page formulaire

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import senior from "@assets/images/senior.webp";
 
 export default function ContactForm() {
   const [contact, setContact] = useState({
@@ -20,71 +21,76 @@ export default function ContactForm() {
   };
 
   return (
-    <div>
-      <h2>Nous contacter</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          postContact();
-        }}
-      >
-        <input
-          type="text"
-          value={contact.lastname}
-          placeholder="Votre Nom"
-          onChange={(e) =>
-            setContact({
-              ...contact,
-              lastname: e.target.value,
-            })
-          }
-        />
-        <input
-          type="text"
-          value={contact.firstname}
-          placeholder="Prénom"
-          onChange={(e) =>
-            setContact({
-              ...contact,
-              firstname: e.target.value,
-            })
-          }
-        />
-        <input
-          type="text"
-          value={contact.pseudo}
-          placeholder="Pseudo"
-          onChange={(e) =>
-            setContact({
-              ...contact,
-              pseudo: e.target.value,
-            })
-          }
-        />
-        <input
-          type="text"
-          value={contact.email}
-          placeholder="Votre adresse mail"
-          onChange={(e) =>
-            setContact({
-              ...contact,
-              email: e.target.value,
-            })
-          }
-        />
-        <input
-          type="text"
-          value={contact.content}
-          placeholder="Votre message"
-          onChange={(e) =>
-            setContact({
-              ...contact,
-              content: e.target.value,
-            })
-          }
-        />
-        <input type="submit" value="Envoyer" />
-      </form>
+    <div className="contain-form">
+      <div className="contain-form1" style={{backgroundImage: `url(${senior})`, backgroundSize:"cover"}}>
+        <h2 className="h2form">Vous souhaitez nous contacter ?</h2>
+        <p className="pform">Remplissez le formulaire ci-contre</p>
+      </div>
+      <div className="contain-form2">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            postContact();
+          }}
+        >
+          <input className="inputform1"
+            type="text"
+            value={contact.lastname}
+            placeholder="Votre Nom"
+            onChange={(e) =>
+              setContact({
+                ...contact,
+                lastname: e.target.value,
+              })
+            }
+          />
+          <input className="inputform2"
+            type="text"
+            value={contact.firstname}
+            placeholder="Prénom"
+            onChange={(e) =>
+              setContact({
+                ...contact,
+                firstname: e.target.value,
+              })
+            }
+          />
+          <input className="inputform3"
+            type="text"
+            value={contact.pseudo}
+            placeholder="Pseudo"
+            onChange={(e) =>
+              setContact({
+                ...contact,
+                pseudo: e.target.value,
+              })
+            }
+          />
+          <input className="inputform4"
+            type="text"
+            value={contact.email}
+            placeholder="Votre adresse mail"
+            onChange={(e) =>
+              setContact({
+                ...contact,
+                email: e.target.value,
+              })
+            }
+          />
+          <input className="inputform5"
+            type="text"
+            value={contact.content}
+            placeholder="Votre message"
+            onChange={(e) =>
+              setContact({
+                ...contact,
+                content: e.target.value,
+              })
+            }
+          />
+          <input type="submit" value="Envoyer" />
+        </form>
+      </div>
     </div>
   );
 }
