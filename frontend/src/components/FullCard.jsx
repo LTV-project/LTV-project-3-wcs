338c5d6a329682a3c5dff04e0d3d760fb115ff72
Tamajin <wild.jlq@gmail.com>
2022-10-05 23:43:29 +0200

    ajout de tous les éléments à afficher, positionnement en grid

--- frontend/src/components/FullCard.jsx
+++ frontend/src/components/FullCard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { transDate } from "@services/DateManager";
 import { useParams } from "react-router-dom";
 
 function FullCard() {
@@ -14,7 +15,7 @@ function FullCard() {
   }, []);
 
   return (
-    <>
+    <div className="full-card">
       <div className="creator-infos">
         <img
           src="https://placekitten.com/g/50/50"
@@ -28,14 +29,34 @@ function FullCard() {
         <p className="mini-card-body">
           Nombre de joueurs : {lobbyFullDetails.number_of_gamers}
         </p>
-        <p className="mini-card-body">
+        <p className="mini-card-body participants">
           Participants : {lobbyFullDetails.participants}
         </p>
       </div>
       <p className="mini-card-body">
         Description : {lobbyFullDetails.description}
       </p>
-    </>
+      <div className="travel-infos">
+        <p className="travel-infos-item6">
+          Date : {transDate(lobbyFullDetails.date)}{" "}
+        </p>
+        <p className="travel-infos-item1">
+          Numéro de train : {lobbyFullDetails.train_number}
+        </p>
+        <p className="travel-infos-item2">
+          Voiture : {lobbyFullDetails.coach_number}{" "}
+        </p>
+        <p className="travel-infos-item3">
+          Place de l'hôte : {lobbyFullDetails.seat_number}{" "}
+        </p>
+        <p className="travel-infos-item4">
+          Gare de départ : {lobbyFullDetails.departure}
+        </p>
+        <p className="travel-infos-item5">
+          Gare d'arrivée : {lobbyFullDetails.arrival}{" "}
+        </p>
+      </div>
+    </div>
   );
 }
 
