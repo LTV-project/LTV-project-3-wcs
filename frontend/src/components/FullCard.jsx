4e965ee0f263a7e1ba31347dc3b3fe1900266c05
Tamajin <wild.jlq@gmail.com>
2022-10-05 14:40:23 +0200

    création de deux nouveaux composants pour gérer l'affichage des salles disponibles, en version mini et en full details

---
+++ frontend/src/components/FullCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function FullCard({ lobby }) {
+  return (
+    <>
+      <div className="creator-infos">
+        <img
+          src="https://placekitten.com/g/50/50"
+          alt="creator-avatar"
+          className="creator-avatar"
+        />
+        <p className="creator-pseudo">Hôte : {lobby.pseudo}</p>
+      </div>
+      <div className="game-infos">
+        <p className="mini-card-body">Jeu : {lobby.game}</p>
+        <p className="mini-card-body">
+          Nombre de joueurs : {lobby.number_of_gamers}
+        </p>
+      </div>
+      <p className="mini-card-body">Participants : {lobby.participants}</p>
+      <p className="mini-card-body">Description : {lobby.description}</p>
+    </>
+  );
+}
+
+export default FullCard;
