d1e31af02de96765d1e990f9782e78f5b3249d05
Tamajin <wild.jlq@gmail.com>
2022-10-06 17:08:13 +0200

    ajout de lignes de css

--- frontend/src/components/FullCard.jsx
+++ frontend/src/components/FullCard.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { transDate } from "@services/DateManager";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Banner from "./Banner";
 
 function FullCard() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [lobbyFullDetails, setLobbyFullDetails] = useState("");
 
   useEffect(() => {
@@ -15,48 +17,66 @@ function FullCard() {
   }, []);
 
   return (
-    <div className="full-card">
-      <div className="creator-infos">
-        <img
-          src="https://placekitten.com/g/50/50"
-          alt="creator-avatar"
-          className="creator-avatar"
-        />
-        <p className="creator-pseudo">Hôte : {lobbyFullDetails.pseudo}</p>
-      </div>
-      <div className="game-infos">
-        <p className="mini-card-body">Jeu : {lobbyFullDetails.game}</p>
+    <>
+      <Banner />
+      <div className="full-card">
+        <div className="creator-infos">
+          <img
+            src="https://placekitten.com/g/50/50"
+            alt="creator-avatar"
+            className="creator-avatar"
+          />
+          <p className="creator-pseudo">Hôte : {lobbyFullDetails.pseudo}</p>
+        </div>
+        <div className="game-infos">
+          <p className="mini-card-body game-title">
+            Jeu : {lobbyFullDetails.game}
+          </p>
+          <p className="mini-card-body nb-players">
+            Nombre de joueurs : {lobbyFullDetails.number_of_gamers}
+          </p>
+          <p className="participants nb-users">
+            Participants : {lobbyFullDetails.participants}
+          </p>
+        </div>
         <p className="mini-card-body">
-          Nombre de joueurs : {lobbyFullDetails.number_of_gamers}
+          Description : {lobbyFullDetails.description}
         </p>
-        <p className="participants">
-          Participants : {lobbyFullDetails.participants}
-        </p>
+        <div className="travel-infos">
+          <p className="travel-infos-item6">
+            Date : {lobbyFullDetails.date && transDate(lobbyFullDetails.date)}{" "}
+          </p>
+          <p className="travel-infos-item1">
+            Numéro de train : {lobbyFullDetails.train_number}
+          </p>
+          <p className="travel-infos-item2">
+            Voiture : {lobbyFullDetails.coach_number}{" "}
+          </p>
+          <p className="travel-infos-item3">
+            Place de l'hôte : {lobbyFullDetails.seat_number}{" "}
+          </p>
+          <p className="travel-infos-item4">
+            Gare de départ : {lobbyFullDetails.departure}
+          </p>
+          <p className="travel-infos-item5">
+            Gare d'arrivée : {lobbyFullDetails.arrival}{" "}
+          </p>
+        </div>
       </div>
-      <p className="mini-card-body">
-        Description : {lobbyFullDetails.description}
-      </p>
-      <div className="travel-infos">
-        <p className="travel-infos-item6">
-          Date : {lobbyFullDetails.date && transDate(lobbyFullDetails.date)}{" "}
-        </p>
-        <p className="travel-infos-item1">
-          Numéro de train : {lobbyFullDetails.train_number}
-        </p>
-        <p className="travel-infos-item2">
-          Voiture : {lobbyFullDetails.coach_number}{" "}
-        </p>
-        <p className="travel-infos-item3">
-          Place de l'hôte : {lobbyFullDetails.seat_number}{" "}
-        </p>
-        <p className="travel-infos-item4">
-          Gare de départ : {lobbyFullDetails.departure}
-        </p>
-        <p className="travel-infos-item5">
-          Gare d'arrivée : {lobbyFullDetails.arrival}{" "}
-        </p>
+      <div className="full-card-btn-wrapper">
+        <button type="button" className="join-btn join-btn-full-card">
+          rejoindre
+        </button>
+        <div className="empty-space" />
+        <button
+          type="button"
+          className="generic-btn details-btn return-lobby-search-btn"
+          onClick={() => navigate(-1)}
+        >
+          Revenir à la recherche
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
