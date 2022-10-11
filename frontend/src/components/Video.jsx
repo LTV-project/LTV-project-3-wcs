d1bbb5fc0a13c16996d268255a23c806529aa458
Tamajin <wild.jlq@gmail.com>
2022-10-11 16:49:17 +0200

    ajout de la video

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,5 +1,24 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import CTA from "@assets/video/CTA-projet3.mp4";
+import { useNavigate } from "react-router-dom";
+
 function Video() {
-  return <div>Video</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="CTA-container">
+      // eslint-disable-next-line jsx-a11y/media-has-caption
+      <video src={CTA} width="540" height="300" controls autoPlay loop />
+      <button
+        type="button"
+        className="CTA-btn generic-btn"
+        onClick={() => navigate("/account-creation")}
+      >
+        Rejoignez nous vite !
+      </button>
+    </div>
+  );
 }
 
 export default Video;
